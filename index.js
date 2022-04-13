8cc0f2d32410a6d8a8e133c996f001931030415b
Aaron Stokes <stokescomp01@gmail.com>
2022-04-13 14:10:25 -0600

    add const/require for inquirer, fs, path, and lib, add lib files per brief

---
+++ index.js
@@ -0,0 +1,6 @@
+const inquire = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+const Manager = require("./lib/Manager.test.js");
+const Engineer = require("./lib/Engineer.test.js");
+const Intern = require("./lib/Manager.test.js");
